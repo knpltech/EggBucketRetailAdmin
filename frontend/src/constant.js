822b20a51a5bfd553dafb09a46194133f5298b69
JRJittu <jrjittu.777@gmail.com>
2025-06-26 19:04:50 +0530

    modified backend path

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,5 @@
 // export const ADMIN_PATH = "http://localhost:3000/api/admin"
-export const ADMIN_PATH = "https://eggbucketretail-backend.onrender.com/api/admin"
+export const ADMIN_PATH = "https://eggbucketretail.onrender.com/api/admin"
 // export const RETAIL_PATH = "http://localhost:3000/api/retail"
-export const RETAIL_PATH = "https://eggbucketretail-backend.onrender.com/api/retail"
+export const RETAIL_PATH = "https://eggbucketretail.onrender.com/api/retail"
 export const GOOGLE_MAP_KEY = "AIzaSyCbRbyJStuI_YFqidGJCBbWvdL6ud5CGCI"
