b779928a4094dcfc8f3341d1520bf74527f83efb
JRJittu <jrjittu.777@gmail.com>
2025-07-08 23:53:01 +0530

    Changed backend path to railway.com

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,8 @@
 // export const ADMIN_PATH = "http://localhost:3000/api/admin"
-export const ADMIN_PATH = "https://eggbucketretailadmin.onrender.com/api/admin"
-
 // export const RETAIL_PATH = "http://localhost:3000/api/retail"
-export const RETAIL_PATH = "https://eggbucketretailadmin.onrender.com/api/retail"
+
+// export const ADMIN_PATH = "https://eggbucketretailadmin.onrender.com/api/admin"
+// export const RETAIL_PATH = "https://eggbucketretailadmin.onrender.com/api/retail"
+
+export const ADMIN_PATH = "https://eggbucketretailadmin-production.up.railway.app/api/admin"
+export const RETAIL_PATH = "https://eggbucketretailadmin-production.up.railway.app/api/retail"
