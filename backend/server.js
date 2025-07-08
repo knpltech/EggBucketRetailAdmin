c687d22c4552bcf8b4865ecc1b58805f9217b57f
JRJittu <jrjittu.777@gmail.com>
2025-07-08 20:36:41 +0530

    Fixed cors errors.

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ admin.initializeApp({
 
 const app = express();
 
+app.use(cors({origin: "*",}));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/admin", adminRouter);
